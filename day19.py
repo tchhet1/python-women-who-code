b641df73950d150e2a39b67e7565a1da4682d782
Tripti Chhetri <triptichhetri312@gmail.com>
2024-01-28 23:27:30 -0600

    day 19 of 60 days of python coding challange

---
+++ day19.py
@@ -0,0 +1,22 @@
+#Write a function to calculate the factorial of a number.
+# 5 = 5*4*3*2*1
+
+#solution 1
+def factorial(num):
+    result = 1
+    for i in range(num, 1, -1):
+        result = result * i
+    print(result)
+    return result
+
+factorial(5)
+
+#solution 2 using recurion
+def factorial1(n):
+    if(n == 0 or n ==1):
+        return 1
+    else:
+        return n * factorial1(n - 1)
+    
+result = factorial1(5)
+print(result)
